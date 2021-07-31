ade927955969b38392cf37366fb5fdbd37ff3ccb
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-07-31 15:43:11 -0300

    tratamento de input + flash

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
+
 
 # Configure application
 app = Flask(__name__)
@@ -10,6 +11,10 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
+# Secret key
+app.secret_key = b'_5#y2L"2dn399q3bfjkbqk23ie3imdd3'
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -18,7 +23,21 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
+        for chave, valor in {'Nome': name, 'Mês': month, 'Dia': day}.items():
+            if not valor:
+                return render_template('erro.html', erro=f"{chave} não digitado!")
+        
+        if int(month) in [4, 6, 9, 11] and int(day) == 31:
+            return render_template('erro.html', erro="O mês digitado não tem dia 31!")
+
+        if int(month) == 2 and int(day) > 29:
+            return render_template('erro.html', erro=f"Fevereiro não tem dia {int(day)}!")
+
+        name = name.strip().capitalize().split()[0]
+
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
+
+        flash(f'Aniversário de {name} adicionado!')
 
         return redirect("/")
 
@@ -27,3 +46,21 @@ def index():
         rows = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html", rows=rows)
+
+@app.route("/email_parabens", methods=["GET", "POST"])
+def email_parabens():
+    if request.method == 'GET':
+        textos, emails = [], []
+        rows = db.execute("SELECT * FROM birthdays")  # ta certo?
+
+        for row in rows:  # iterar sobre as rows da db
+            if ... == ...:  # data da row == data de hoje
+                nome = ...
+
+            texto = f'Hoje é o aniversário de {nome}! Clique aqui para enviar os parabéns.'
+            email = ...
+
+            textos.append(texto)
+            emails.append(email)
+
+        return render_template('email_parabens.html', textos=textos, emails=emails)
